e94315e1db5a043818f2acb375e016009fda1d6b
avtohem <36834485+avtohem@users.noreply.github.com>
2019-12-15 18:39:57 +0500

    type check

---
+++ checkClass.groovy
@@ -0,0 +1,8 @@
+//check class of object
+
+Map obj = [:]
+println obj.getClass() //return EXACT type
+
+//instance of - check if object is a subclass of class
+println obj instanceof Map //true 
+println obj instanceof LinkedHashMap //true 
